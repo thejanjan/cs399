6dc88edf47bf20fcf35022a94cf2f366fce11a6b
sheep <82494123+thejanjan@users.noreply.github.com>
2024-02-09 19:09:24 -0800

    hw2: pg1 improve output

--- hw2/program1/axpy.c
+++ hw2/program1/axpy.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 ///
 /// Benchmark functions
@@ -24,6 +25,22 @@ void end_benchmark(char *name, int array_size) {
 /// Main
 ///
 
+void verify_results(float **out, int n, float a) {
+	bool success = true;
+	float output_val = (2.0f * 2.0f) + 1.0f;  // a*x+y
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (out[i][j] != output_val) {
+				printf("Verify Failure - y[%f][%f] == %f and not %f\n", i, j, out[i][j], output_val);
+				success = false;
+			}
+		}
+	}
+	if (success) {
+		printf("Output verified.\n");
+	}
+}
+
 int main(int argc, char *argv[]) {
     // Handle option inputs -- really rough arg parsing
     int n = 1000;
@@ -42,6 +59,46 @@ int main(int argc, char *argv[]) {
         x[i] = (float *)malloc(n * sizeof(float));
         y[i] = (float *)malloc(n * sizeof(float));
     }
+	
+	///
+	/// Combination #1
+	/// No clauses
+	///
+
+    // Data region for 2d arrays on GPU
+    start_benchmark();
+    #pragma acc data create(x[:n][:n], y[:n][:n])
+    {
+        // Set float values on GPU
+        #pragma acc parallel loop
+        for (int i = 0; i < n; i++) {
+            #pragma acc loop
+            for (int j = 0; j < n; j++) {
+                x[i][j] = 1.0f;
+                y[i][j] = 2.0f;
+            }
+        }
+
+        // Compute sum over array
+        #pragma acc parallel loop
+        for (int i = 0; i < n; i++) {
+            #pragma acc loop
+            for (int j = 0; j < n; j++) {
+                y[i][j] = a*x[i][j]+y[i][j];
+            }
+        }
+
+        // Copy result back to CPU
+        #pragma acc update self(x[:n][:n], y[:n][:n])
+    }
+	
+    end_benchmark("GPU process time - no clauses", n);
+	verify_results(y, n, a);
+	
+	///
+	/// Combination #2
+	/// Gang/Vector
+	///
 
     // Data region for 2d arrays on GPU
     start_benchmark();
@@ -69,7 +126,79 @@ int main(int argc, char *argv[]) {
         // Copy result back to CPU
         #pragma acc update self(x[:n][:n], y[:n][:n])
     }
-    end_benchmark("GPU process time", n);
+	
+	end_benchmark("GPU process time - gang outer, vector inner", n);
+	verify_results(y, n, a);
+	
+	///
+	/// Combination #3
+	/// Gang/Worker
+	///
+
+    // Data region for 2d arrays on GPU
+    start_benchmark();
+    #pragma acc data create(x[:n][:n], y[:n][:n])
+    {
+        // Set float values on GPU
+        #pragma acc parallel loop gang
+        for (int i = 0; i < n; i++) {
+            #pragma acc loop worker
+            for (int j = 0; j < n; j++) {
+                x[i][j] = 1.0f;
+                y[i][j] = 2.0f;
+            }
+        }
+
+        // Compute sum over array
+        #pragma acc parallel loop gang
+        for (int i = 0; i < n; i++) {
+            #pragma acc loop worker
+            for (int j = 0; j < n; j++) {
+                y[i][j] = a*x[i][j]+y[i][j];
+            }
+        }
+
+        // Copy result back to CPU
+        #pragma acc update self(x[:n][:n], y[:n][:n])
+    }
+	
+	end_benchmark("GPU process time - gang outer, worker inner", n);
+	verify_results(y, n, a);
+	
+	///
+	/// Combination #4
+	/// Worker/Vector
+	///
+
+    // Data region for 2d arrays on GPU
+    start_benchmark();
+    #pragma acc data create(x[:n][:n], y[:n][:n])
+    {
+        // Set float values on GPU
+        #pragma acc parallel loop worker
+        for (int i = 0; i < n; i++) {
+            #pragma acc loop vector
+            for (int j = 0; j < n; j++) {
+                x[i][j] = 1.0f;
+                y[i][j] = 2.0f;
+            }
+        }
+
+        // Compute sum over array
+        #pragma acc parallel loop worker
+        for (int i = 0; i < n; i++) {
+            #pragma acc loop vector
+            for (int j = 0; j < n; j++) {
+                y[i][j] = a*x[i][j]+y[i][j];
+            }
+        }
+
+        // Copy result back to CPU
+        #pragma acc update self(x[:n][:n], y[:n][:n])
+    }
+	
+	end_benchmark("GPU process time - worker outer, vector inner", n);
+	verify_results(y, n, a);
 
     // cleanup and return
     for (int i = 0; i < n; i++) {
