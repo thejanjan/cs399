f8e381e2a51b01b9549d7d8c3f78dd3b03807ef6
bluerat <micahanichols27@gmail.com>
2024-03-09 13:29:40 -0800

    hw4: setup pg1 array, fix formatting

--- hw4/program1/axpy.cu
+++ hw4/program1/axpy.cu
@@ -6,8 +6,8 @@
 
 const int THREADS = 32;
 
-__global__ void axpy(float *x, float *y, int n, float a) {
-    // calculate the x and y index the thread is working on
+__global__ void setup_axpy(float *x, float *y, int n) {
+	// calculate the x and y index the thread is working on
 	int xi = threadIdx.x + blockIdx.x*blockDim.x;
 	int yi = threadIdx.y + blockIdx.y*blockDim.y;
 	
@@ -15,6 +15,21 @@ __global__ void axpy(float *x, float *y, int n, float a) {
 		// get index
 		int i = xi + (yi * n);
 		
+		// fill with initial values
+		x[i] = 1.0f;
+		y[i] = 2.0f;
+	}
+}
+
+__global__ void axpy(float *x, float *y, int n, float a) {
+	// calculate the x and y index the thread is working on
+	int xi = threadIdx.x + blockIdx.x*blockDim.x;
+	int yi = threadIdx.y + blockIdx.y*blockDim.y;
+
+	if (xi < n && yi < n) {
+		// get index
+		int i = xi + (yi * n);
+
 		// do axpy
 		y[i] = a*x[i]+y[i];
 	}
@@ -27,7 +42,7 @@ void verify_results(float *out, int n, float a) {
 		for (int j = 0; j < n; j++) {
 			if (out[j + (i * n)] != output_val) {
 				printf("Verify Failure - at y[%d]\n", j + (i * n));
-                printf("%f != %f\n", out[j + (i * n)], output_val);
+				printf("%f != %f\n", out[j + (i * n)], output_val);
 				success = false;
 			}
 		}
@@ -38,44 +53,42 @@ void verify_results(float *out, int n, float a) {
 }
 
 int main(int argc, char *argv[]) {
-    // Handle option inputs -- really rough arg parsing
-    int n = 1000;
-    float a = 2.0f;
+	// Handle option inputs -- really rough arg parsing
+	int n = 1000;
+	float a = 2.0f;
 
-    if (argc == 3) {
-        if (strcmp(argv[1], "-n") == 0) {
-            n = atoi(argv[2]);
-        }
-    }
+	if (argc == 3) {
+		if (strcmp(argv[1], "-n") == 0) {
+			n = atoi(argv[2]);
+		}
+	}
 
-    ///
-    /// AXPY operation
-    ///
+	///
+	/// AXPY operation
+	///
 
-    // setup cuda
+	// setup cuda
 	dim3 threads_per_block(THREADS, THREADS);
 	dim3 blocks_per_grid(ceil((float)n/(float)THREADS), ceil((float)n/(float)THREADS));
-	
-    float *x_d, *y_d;
-    float *x = (float *)malloc(n * n * sizeof(float));
+
+	float *x_d, *y_d;
 	float *y = (float *)malloc(n * n * sizeof(float));
-    cudaMalloc(&x_d, n * n * sizeof(float));
+	cudaMalloc(&x_d, n * n * sizeof(float));
 	cudaMalloc(&y_d, n * n * sizeof(float));
 
-    // perform kernel
-    axpy<<<blocks_per_grid, threads_per_block>>>(x_d, y_d, n, a);
+	// perform kernels
+	setup_axpy<<<blocks_per_grid, threads_per_block>>>(x_d, y_d, n);
+	axpy<<<blocks_per_grid, threads_per_block>>>(x_d, y_d, n, a);
 
-    // collect result
-    cudaMemcpy(x, x_d, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
+	// collect result
 	cudaMemcpy(y, y_d, sizeof(float) * n * n, cudaMemcpyDeviceToHost);
-    cudaFree(x_d);
+	cudaFree(x_d);
 	cudaFree(y_d);
 
-    // verify results
+	// verify results
 	verify_results(y, n, a);
 
-    // return and cleanup
-    free(x);
+	// return and cleanup
 	free(y);
     return 0;
 }
